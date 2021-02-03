fbd4b3d326fe6c27354fb90d420b3460b1bce6c3
LolXpro <76632611+LolXpro@users.noreply.github.com>
2021-02-03 13:30:05 +0100

    Authentification

---
+++ auth.js
@@ -0,0 +1,8 @@
+//XHR Authentification
+var xhr = new XMLHttpRequest();
+var url = "https://stundenplan.berufskolleg-bottrop.de/schueler/",
+ user = "schueler",
+ pass = "stundenplan";
+
+xhr.open("POST", url, true, user, pass);
+xhr.send();
